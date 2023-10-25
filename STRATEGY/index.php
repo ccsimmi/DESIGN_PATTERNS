45dd16022589280b2ce1522f22f38ca765a70d75
ccsimmi <ffsimmi@gmail.com>
2023-10-25 18:56:56 +0100

    index.php file

--- STRATEGY/index.php
+++ STRATEGY/index.php
@@ -1,2 +1,67 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+use App\Ducks\MallardDuck;
+use App\Ducks\MarbledDuck;
+use App\Ducks\RubberDuck;
+
+$mallard = new MallardDuck();
+$marbled = new MarbledDuck();
+$rubber = new RubberDuck();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ducks with strategies</title>
+    <link rel="stylesheet" href="public/styles.css" />
+</head>
+<body>
+    <h1>Strategy pattern with ducks</h1>
+    <div class="container">
+        <div class="card">
+            <img alt="Mallard duck" src="public/images/mallard.jpg" />
+            <div>
+                <p class="card__title"><?= $mallard->getDuckName() ?></p>
+                <p class="card__description"><?= $mallard->describeDuck() ?></p>
+                <div class="card__stats">
+                    <p>Stats</p>
+                    <p><?= $mallard->performFly() ?></p>
+                    <p><?= $mallard->performQuack() ?></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img alt="Marbled duck" src="public/images/marbled.jpeg" />
+            <div>
+                <p class="card__title"><?= $marbled->getDuckName() ?></p>
+                <p class="card__description"><?= $marbled->describeDuck() ?></p>
+                <div class="card__stats">
+                    <p>Stats</p>
+                    <p><?= $marbled->performFly() ?></p>
+                    <p><?= $marbled->performQuack() ?></p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card">
+            <img alt="Rubber duck" src="public/images/rubber.jpg" />
+            <div>
+                <p class="card__title"><?= $rubber->getDuckName() ?></p>
+                <p class="card__description"><?= $rubber->describeDuck() ?></p>
+                <div class="card__stats">
+                    <p>Stats</p>
+                    <p><?= $rubber->performFly() ?></p>
+                    <p><?= $rubber->performQuack() ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+
